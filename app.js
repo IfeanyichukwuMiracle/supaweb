f78975471dfd92dd918ad11cb97045d89ba98598
IfeanyiMiracleIfeanyi <ifeanyimiraclechuks01@gmail.com>
2024-04-15 11:37:38 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const port = process.env.PORT || 5000;
+startServer(process.env.MONGO_LOCAL_URI);
+
+// middleware
+app.use(express.json());
+app.use(express.static(`./public`));
+
+app.get("/route", (req, res) => {
+  res.send("Hello");
+});
+
+async function startServer(db_url) {
+  try {
+    await mongoose.connect(db_url);
+    console.log(`DB connected...`);
+    app.listen(port, () => console.log(`Server running on port ${port}...`));
+  } catch (error) {
+    console.log(`Error => ${error}`);
+  }
+}
